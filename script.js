614f85290e1190717df4bd1f692ffc7dd31e1798
Georgina <georginabosque@gmail.com>
2022-05-18 22:37:15 -0300

    clase03 completada

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ const btn = document.querySelector('[data-form-btn]');
 const creatTask = (evento) =>{
 	evento.preventDefault(); // evita se recarge la pagina 
 	const input = document.querySelector('[data-form-input]');
-	console.log(input.value);
+	const value = input.value;
+	//const task = document.querySelector('[date-task]');
+	const list = document.querySelector('[data-list]');
+	const task = document.createElement("li");
+	task.classList.add("card");
+	input.value = "" // para limpiar el imput
+	//agregamos codigo de javascript dentro del html ${}
+	const content = `<div>
+              <i class="far fa-check-square icon"></i>
+              <span class="task">${value}</span> 
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>`;        
+    task.innerHTML = content // ponemos la info del usuario que pone en el input en nuestra card
+    
+    list.appendChild(task);
+
+	console.log(content);
 }
 
 //Arrow functions o funciones anonimas 
