a046a26621dab72c80acc7a1a95532852a88d5d0
Georgina <georginabosque@gmail.com>
2022-05-19 22:42:54 -0300

    clases04 completada

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector('[data-form-btn]');
+(() => {
+ const btn = document.querySelector('[data-form-btn]');
 
 const creatTask = (evento) =>{
 	evento.preventDefault(); // evita se recarge la pagina 
@@ -8,19 +9,41 @@ const creatTask = (evento) =>{
 	const list = document.querySelector('[data-list]');
 	const task = document.createElement("li");
 	task.classList.add("card");
-	input.value = "" // para limpiar el imput
+	input.value = "" // para limpiar el input
 	//agregamos codigo de javascript dentro del html ${}
-	const content = `<div>
-              <i class="far fa-check-square icon"></i>
-              <span class="task">${value}</span> 
-            </div>
+	const taskContent = document.createElement("div")
+	taskContent.appendChild(checkComplete())
+	const titleTask = document.createElement("spna")
+	titleTask.classList.add("task")
+	titleTask.innerText = value;
+	taskContent.appendChild(titleTask);
+
+	const content = `          
             <i class="fas fa-trash-alt trashIcon icon"></i>`;        
-    task.innerHTML = content // ponemos la info del usuario que pone en el input en nuestra card
-    
+    //task.innerHTML = content // ponemos la info del usuario que pone en el input en nuestra card
+    task.appendChild(taskContent);
+
     list.appendChild(task);
 
-	console.log(content);
 }
 
 //Arrow functions o funciones anonimas 
-btn.addEventListener("click", creatTask)
+btn.addEventListener("click", creatTask);
+
+const checkComplete = () =>{
+	const i = document.createElement("i"); // genero <i class="far fa-check-square icon"></i>
+	i.classList.add('far','fa-check-square','icon');
+	i.addEventListener("click",completeTask);
+	return i;
+}
+
+//Immediatly invoked function expression IIFE => el usuario no debe tener acceso a nuestras funciones 
+const completeTask = (event) => {
+	const element = event.target;
+	element.classList.toggle("fas");
+	element.classList.toggle("completeIcon");// clase que genere yo para ponerle colo al icono
+	element.classList.toggle("far");
+
+};
+
+})();
